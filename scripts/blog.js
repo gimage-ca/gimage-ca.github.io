505d25e4f4c6aa8c31ea295efa766dd29bd2bc2b
Amir Mardan <46511946+AmirMardan@users.noreply.github.com>
2024-07-16 17:44:28 -0400

    init

---
+++ scripts/blog.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    // Handler for .ready() called.
+    $('html, body').animate({
+        scrollTop: $('#research').offset().top
+    }, 'slow');
+});
---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+window.addEventListener('scroll', function() {
+    const logo = document.querySelector('.logo'); // Adjust the selector to target your logo
+    
+    if (window.scrollY > 10) { // Change 50 to the scroll threshold
+        logo.src = '../img/logo_wb_small.svg'; // Path to the logo to use after scrolling
+        // console.log(window.scrollY);
+    } else {
+        logo.src = '../img/logo_small.svg'; // Path to the original logo
+    }
+  });
+
+// JS written by Themetide
+
+
+
+
+
